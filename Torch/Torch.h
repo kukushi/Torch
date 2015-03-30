98d9a71e48ececa42b9d59cfa1b10fca51b85bc0
kukushi <17washabi@gmail.com>
2015-03-30 17:48:19 +0800

    Initial Commit

---
+++ Torch/Torch.h
@@ -0,0 +1,19 @@
+//
+//  Torch.h
+//  Torch
+//
+//  Created by kukushi on 3/30/15.
+//  Copyright (c) 2015 Xing He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Torch.
+FOUNDATION_EXPORT double TorchVersionNumber;
+
+//! Project version string for Torch.
+FOUNDATION_EXPORT const unsigned char TorchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Torch/PublicHeader.h>
+
+
